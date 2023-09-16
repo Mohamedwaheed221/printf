e82614243e8510d568953711efc4a807fa3fd205
Mohamedwaheed221 <panther221b@gmail.com>
2023-09-16 04:44:06 -0700

    s

---
+++ main.h
@@ -0,0 +1,19 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/***/
+
+typedef struct print
+{
+	char *type_arg;
+	int (*f)(va_list, char *, unsigned int);
+} print_t;
+
+int _printf(const char *format, ...);
+int print_chr(va_list arguments, char *buf, unsigned int ibuf);
+
+#endif
