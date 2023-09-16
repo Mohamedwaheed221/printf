22e6b1078b150ce273c5437c4eacc014f4f7ae39
Mohamedwaheed221 <panther221b@gmail.com>
2023-09-16 05:37:53 -0700

    1

--- main.h
+++ main.h
@@ -13,6 +13,8 @@ typedef struct print
 	int (*f)(va_list, char *, unsigned int);
 } print_t;
 
+int putchar(char c);
+int puts(const char *str)
 int _printf(const char *format, ...);
 int print_chr(va_list arguments, char *buf, unsigned int ibuf);
 
