5ad62cf50f3af2b99b41f3d2ea950014790724a1
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-09-16 13:14:27 -0700

    ./a

--- _printf.c
+++ _printf.c
@@ -1,26 +1,15 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdarg.h>
 #include "main.h"
-
-/**
- * _printf - function that prints any type of arguments
- * @format: string
-=======
-#include "main.h"
-#include <stdio.h>
-#include <stdarg.h>
 
 /**
  * _printf - function that print any argument
  * @format: pointer to a string
->>>>>>> 4062355999eb34edaf2850eff395f42d8e4f57f8
  * i: input
  * Return: i
  */
 int _printf(const char *format, ...)
 {
-<<<<<<< HEAD
 	int i = 0;
 
 	va_list args;
@@ -35,7 +24,7 @@ int _printf(const char *format, ...)
 			if (*format == 'c')
 			{
 			i++;
-			i = printf("%c", va_arg(args, int));
+			printf("%c", va_arg(args, int));
 			}
 			else if (*format == 's')
 			{
@@ -61,32 +50,3 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (i);
 }
-
-=======
-        int i = 0;
-
-        va_list args;
-
-        va_start(args, format);
-
-        while (*format != '\0')
-        {
-                if (*format == 'c')
-                {
-                        i += putchar(va_arg(args, int));
-                }
-                else if (*format == 's')
-                {
-                        i += puts(va_arg(args, char*));
-                }
-                else
-                {
-                        i += putchar ('%');
-                }
-                format++;
-        }
-        va_end(args);
-
-        return (i);
-}
->>>>>>> 4062355999eb34edaf2850eff395f42d8e4f57f8
