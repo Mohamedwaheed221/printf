ebdaa35a8859a3b366f8edbd435c376fcd8bb68c
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-09-17 12:39:40 -0700

    main.h

--- main.h
+++ main.h
@@ -8,29 +8,15 @@
 #include <limits.h>
 
 /**
-<<<<<<< HEAD
  * _printf - function that prints any argument
-=======
- * _printf - printf function
->>>>>>> 557c5632125647bd227e6e9f67f762f67c7fa880
  * puts - function to print char
- * _putchar - putchar function
  * @format: const char
  *
-<<<<<<< HEAD
  * Return: a data type value
  */
 
 int _printf(const char *format, ...);
 int puts(const char*str);
-=======
- * Return: a datatype value.
- */
 
-int _printf(const char *format, ...);
-int puts(const char *str);
-int putchar(char c);
-
->>>>>>> 557c5632125647bd227e6e9f67f762f67c7fa880
 
 #endif
