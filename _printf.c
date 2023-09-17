2a23bbaa817fc12a577812102a4916150387094f
Mohamedwaheed221 <panther221b@gmail.com>
2023-09-17 03:57:24 -0700

    1

--- _printf.c
+++ _printf.c
@@ -3,11 +3,8 @@
  * _printf - function that print any argument
  * @format: pointer to a string
  * spacifier: input
-<<<<<<< HEAD
  * Ruturn: spacifier
-=======
  * Return: spacifier
->>>>>>> f5cc4613f00e2e9a3113bf60a823154a567c064c
  */
 
 int _printf(const char *format, ...)
@@ -15,11 +12,7 @@ int _printf(const char *format, ...)
 	int spacifier = 0;
 	va_list args;
 	va_start(args, format);
-<<<<<<< HEAD
-	while(*format)
-=======
 	while (*format)
->>>>>>> f5cc4613f00e2e9a3113bf60a823154a567c064c
 	{
 		if(*format == '%')
 		{
@@ -27,7 +20,6 @@ int _printf(const char *format, ...)
 			switch (*format)
 			{
 				case 'c':
-<<<<<<< HEAD
 				spacifier += putchar(va_arg(args, int));
 				break;
 				case 's':
@@ -36,7 +28,6 @@ int _printf(const char *format, ...)
 				case '%':
 				spacifier += putchar('%');
 				break;
-=======
 					spacifier += putchar(va_arg(args, int));
 					break;
 				case 's':
@@ -45,7 +36,6 @@ int _printf(const char *format, ...)
 				case '%':
 					spacifier += putchar('%');
 					break;
->>>>>>> f5cc4613f00e2e9a3113bf60a823154a567c064c
 			}
 			format++;
 		}
@@ -53,16 +43,13 @@ int _printf(const char *format, ...)
 		{
 			format++;
 			spacifier += putchar(*format);
-<<<<<<< HEAD
 		}
 	}
 	va_end(args);
 	return (spacifier);
 }
-=======
 		}
 		}
 	va_end(args);
 	return (spacifier);
-}		
->>>>>>> f5cc4613f00e2e9a3113bf60a823154a567c064c
+}
