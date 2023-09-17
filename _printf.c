6235d966c2e22c483f2c98c3013c9a57180cc395
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-09-17 05:01:07 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -18,23 +18,23 @@ int _printf(const char *format, ...)
 			switch (*format)
 			{
 				case 'c':
-					specifier += putchar(va_arg(args, int));
+					spacifier += putchar(va_arg(args, int));
 					break;
 				case 's':
-					specifier += puts(va_arg(args, char*));
+					spacifier += puts(va_arg(args, char*));
 					break;
 				case '%':
-					specifier += putchar('%');
+					spacifier += putchar('%');
 					break;
 			}
 			format++;
 		}
 		else
 		{
-			specifier += putchar(*format);
+			spacifier += putchar(*format);
 			format++;
 		}
 	}
 	va_end(args);
-	return (specifier);
+	return (spacifier);
 }
