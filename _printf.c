6cdb36c8a10f22e797d6dfb896e0de8fbd8999ba
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-09-17 12:30:12 -0700

    printf

---
+++ _printf.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * _printf - function that print any argument
+ * @format: char
+ * i: int
+ * len: length of string
+ * str: array
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0;
+	int len = 0;
+	char str[1000];
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					str[len++] = putchar(va_arg(args, int));
+					break;
+				case 's':
+					str[len++] = puts(va_arg(args, char*));
+					break;
+				case '%':
+					str[len++] = putchar('%');
+					break;
+			}
+			i++;
+		}
+		else
+		{
+			str[len++] = putchar(format(i++));
+		}
+	}
+	va_end(args);
+	return (len);
+}
---
+++ main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * _printf - function that prints any argument
+ * puts - function to print char
+ * _putchar - putchar function
+ * @format: const char
+ *
+ * Return: a data type value
+ */
+
+int _printf(const char *format, ...);
+int puts(const char*str);
+
+#endif
