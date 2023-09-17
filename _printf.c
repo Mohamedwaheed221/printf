e3290c9c355b284fed3b30aca5a52dba88f7ee03
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-09-17 07:19:24 -0700

    printf function

---
+++ _printf.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * _printf - function that prints any argument
+ * @format: sting
+ * spacifier: input
+ * Return: spacifier
+ */
+int _printf(const char *format, ...)
+{
+	int spacifier = 0;
+	va_list args;
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				spacifier++;
+				printf("%c",va_arg(args, int));
+			}
+			else if (*format == 's')
+			{
+				spacifier += printf("%s",va_arg(args, char*));
+			}
+			else if (*format == '%')
+			{
+				spacifier++;
+				putchar('%');
+			}
+		}
+			else
+			{
+				spacifier++;
+				putchar(*format);
+			}
+			spacifier++;
+		}
+	va_end(args);
+	return (spacifier);
+}
