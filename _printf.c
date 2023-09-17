d7386bea56e0581671198dd4734fc58834505fc2
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-09-17 02:22:00 -0700

    1

--- _printf.c
+++ _printf.c
@@ -1,7 +1,4 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "main.h"
-
 /**
  * _printf - function that print any argument
  * @format: pointer to a string
@@ -11,28 +8,19 @@
 int _printf(const char *format, ...)
 {
 	int i = 0;
-
 	va_list args;
-
 	va_start(args, format);
-
-	while (*format)
-	{
-		if (*format == '%')
-		{
+	while (*format){
+		if (*format == '%'){
 			format++;
 			if (*format == 'c')
 			{
 			i++;
-			printf("%c", va_arg(args, int));
+			putchar(va_arg(args, int));
 			}
 			else if (*format == 's')
 			{
-				i += printf("%s", va_arg(args, char*));
-			}
-			else if (*format == 'd')
-			{
-				i += printf("%d", va_arg(args, int));
+				i += puts(va_arg(args, char*));
 			}
 			else if (*format == '%')
 			{
