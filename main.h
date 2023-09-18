2abec17696f2a4a443d72a22916fd0c15d8edf89
mohammed6699 <mohammedahmed.refaat@gmail.com>
2023-09-18 04:47:45 -0700

    main.h

--- main.h
+++ main.h
@@ -1,16 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
-<<<<<<< HEAD
 #include <stdlib.h>
 #include <limits.h>
 #include <string.h>
-=======
->>>>>>> refs/remotes/origin/master
 
 
-int putchar(char c);
+int puts(const char *c);
 int _printf(const char *format, ...);
 #endif
